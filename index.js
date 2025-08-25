8aebe0db15d2cbc2d4aea60ada0737229418684b
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-25 00:42:13 -0400

    added middleware and mounted book routes in index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+const bookRoutes = require('./routes/bookRoutes')
+
 app.use(express.json())
 
 
-app.use('/api/books')
+app.use('/api/books', bookRoutes)
 
 
 const PORT = process.env.PORT || 8888
