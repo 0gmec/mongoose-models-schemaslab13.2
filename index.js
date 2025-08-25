591bf482e8ac9e5b80b6d735fd6ed34adc5455ad
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-25 00:38:45 -0400

    need to add validation for book ids

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
+app.use(express.json())
+
+
+app.use('/api/books')
+
+
+const PORT = process.env.PORT || 8888
+app.listen(PORT, ()=> console.log(`Server is running at http://localhost:${PORT}`))
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/Book')
 
-app.post('/', async(req,res)=>{
+router.post('/', async(req,res)=>{
     try{
         const newBook = await Book.create(req.body)
         res.status(201).json(newBook)
@@ -11,7 +11,7 @@ app.post('/', async(req,res)=>{
     }
 })
 
-app.get('/', async (req,res)=> {
+router.get('/', async (req,res)=> {
     try{
         const allBooks = await Book.find({})
         res.status(201).json(allBooks)
@@ -19,3 +19,32 @@ app.get('/', async (req,res)=> {
 res.status(400).json({error: 'Failed to find all books'})
     }
 })
+
+router.get('/:id', async(req,res)=> {
+    try{
+        const bookId = await Book.findById(req.body)
+        res.status(201).json(bookId)
+    }catch (error){
+        res.status(400).json({error: 'Failed to find book'})
+    }
+})
+
+router.put('/:id', async(req,res)=> {
+    try{
+        const updateBookId = await Book.findByIdAndUpdate(bookIdToUpdate, req.body)
+        res.status(201).json(updateBookId)
+    }catch (error) {
+        res.status(400).json({error: 'Failed to update book'})
+    }
+})
+
+router.delete('/:id', async(req,res)=> {
+    try{
+        const deleteBookId = await Book.findByIdAndDelete()
+        res.status(201).json(deleteBookId)
+    }catch (error) {
+        res.status(400).json({error: 'Failed to delete book'})
+    }
+})
+
+module.exports = router
